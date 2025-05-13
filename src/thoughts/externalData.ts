884aeeec6ebd3e14e59fafca0a0071b0a3fc8b54
salil-naik <salil.naik27@gmail.com>
2025-05-13 12:29:57 +0530

    update: thought++

--- src/thoughts/externalData.ts
+++ src/thoughts/externalData.ts
@@ -1,23 +1,31 @@
 import { ThoughtItemProps } from "components/components/ThoughtItem";
 
-export const externalData: ThoughtItemProps[] = [{
-    title: "How I executed Goa Government's ambitious plan to take the Degree and Diploma Admissions online.",
+export const externalData: ThoughtItemProps[] = [
+  {
+    title: "Whatever I build is a reflection of who I am.",
+    date: "2025/05/12",
+    url: "https://x.com/0xSalilNaik/status/1921924680006434819",
+  },
+  {
+    title:
+      "How I executed Goa Government's ambitious plan to take the Degree and Diploma Admissions online.",
     date: "2021/09/01",
     url: "https://medium.com/@salil.naik27/how-i-executed-goa-governments-ambitious-plan-to-take-the-degree-and-diploma-admissions-online-46e34bff43b8",
-},
-{
-    title: "The uncanny relationship between position fixed and transform property.",
+  },
+  {
+    title:
+      "The uncanny relationship between position fixed and transform property.",
     date: "2021/06/05",
     url: "https://dev.to/salilnaik/the-uncanny-relationship-between-position-fixed-and-transform-property-32f6",
-},
-{
+  },
+  {
     title: "My Ethereum India Fellowship Journey",
     date: "2021/05/21",
     url: "https://medium.com/@salil.naik27/my-ethereum-india-fellowship-journey-1637a9fe9e95",
-},
-{
+  },
+  {
     title: "My HackHarvard 2019 Experience",
     date: "2020/09/10",
     url: "https://medium.com/@salil.naik27/my-hackharvard-2019-experience-da1b64f4e5c5",
-},
-]
+  },
+];
